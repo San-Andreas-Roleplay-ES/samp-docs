306ba9cf9b2459fa2733d9b80efd773de5feed48
DoNathaniel <vicente.nathaniel.h@gmail.com>
2026-04-21 12:59:33 -0400

    feat: actualizar página principal del sitio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,23 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import React from 'react';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-
 import styles from './index.module.css';
+import Link from '@docusaurus/Link';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+export default function Home() {
   return (
     <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      title="Inicio"
+      description="Documentación de San Andreas Roleplay">
+      <section className={styles.welcome}>
+        <img src="/img/sarp/logo.png" alt="Logo SARP" className={styles.logo} />
+        <h1 className={styles.title}>Bienvenido a la Docs de SARP</h1>
+        <p className={styles.subtitle}>
+          Aquí encontrarás toda la información sobre el servidor.
+        </p>
+        <Link to="/docs/inicio" className={styles.button}>
+          Ver documentación →
+        </Link>
+      </section>
     </Layout>
   );
 }
